a278f15e8f2399ab88373220bf967dd42e67e830
Abhishek Vaghela <vaghelaabhishek2580@gmail.com>
2022-12-30 19:20:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import LoginForm from './components/LoginForm';
 function App() {
   if (!localStorage.getItem('username')) return <LoginForm />;
   return (
-    <ChatEngine
-      height="100vh"
-      projectID="ed0949d1-023e-4b08-9c07-4c47bae9c568"
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-    />
+    <div className="body">
+      <ChatEngine
+        height="100vh"
+        projectID="ed0949d1-023e-4b08-9c07-4c47bae9c568"
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
+    </div>
   );
 }
 
